5a1c6a3985ad79e9a3033d0bcb6a9a9c1508cdbe
Debanjan Poddar <debanjan2602@gmail.com>
2023-01-02 21:28:18 +0530

    updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
 
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
 
 const Cart = props => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${Math.max(cartCtx.totalAmount, 0).toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({ ...item, amount: 1 });
+    };
+
     const cartItems = (
         <ul className={styles["cart-items"]}>
-            {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-                item => (
-                    <li>{item.name}</li>
-                )
-            )}
+            {cartCtx.items.map(item => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
         </ul>
     );
 
@@ -19,7 +40,7 @@ const Cart = props => {
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button
@@ -28,7 +49,7 @@ const Cart = props => {
                 >
                     Close
                 </button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     );
